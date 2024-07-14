2cf1cdd03afb757445a168968d52e108d05ed78a
Ritesh Bisht <rb1342003@gmail.com>
2024-07-14 21:35:03 +0530

    DAY 1 and DAY 2 Challenge Completed

---
+++ Day1/tasksDay1.js
@@ -0,0 +1,48 @@
+// ACTIVITY 1 : Variable Declaration
+var vNum = 94
+console.log(` var vNum : ${vNum}`)
+
+let lString = "Ritesh"
+console.log(` let lString : ${lString}`)
+
+// ACTIVITY 2 : Conasatant Declaration
+const CBoolean = false;
+console.log(` const CBoolean : ${CBoolean}`)
+
+// ACTIVITY 3 : Data Types
+var num = 9
+var str = "Hello World"
+var bool = false
+var obj = {
+    name :"Ritesh",
+    lastName:"Bisht",
+    age:1,
+    eyeColor:2
+}
+var arr = [ 1,2,"rb",4,true,'h',6.9]
+
+console.log(num)
+console.log(str)
+console.log(bool)
+console.log(obj)
+console.log(arr)
+
+
+// ACTIVITY 4 : Reassigning Values
+let v = 4
+console.log(v)
+v=6
+console.log(v)
+
+// ACTIVITY 5 : Understanding const
+try {
+    const c = 4
+    console.log(`const c : ${c}`)
+    c=6
+} catch (error) {
+    console.log(error);
+}
+finally{
+    console.log(`Constant Can't modified `);
+}
+
